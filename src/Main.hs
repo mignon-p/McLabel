115138147040fc6fc5c1471bc978d26b84965993
Patrick Pelletier <code@funwithsoftware.org>
2021-01-14 00:26:58 -0800

    Add TransformItem module.

--- src/Main.hs
+++ src/Main.hs
@@ -7,40 +7,25 @@ import Text.Read
 
 import MakeLabel
 import McMC
+import TransformItem
+
+prefix :: FilePath
+prefix = "/Users/ppelleti/misc/"
 
 htmlfile :: FilePath
-htmlfile = "/Users/ppelleti/misc/0104ppelleti.html"
+htmlfile = prefix ++ "0104ppelleti.html"
 
-fixCatNo :: LineItem -> LineItem
-fixCatNo item@(LineItem { liCatNo = "", liGotBreak = False }) =
-  item { liDesc = ""
-       , liCatNo = liDesc item
-       }
-fixCatNo item = item
+destDir :: FilePath
+destDir = "/tmp/labels/"
 
-trim :: String -> String
-trim s = dropWhile isSpace $ dropWhileEnd isSpace s
-
-trimItem :: LineItem -> LineItem
-trimItem item =
-  item { liTitle  = trim (liTitle item)
-       , liDesc   = trim (liDesc item)
-       , liCatNo  = trim (liCatNo item)
-       , liLineNo = trim (liLineNo item)
-       , liPoNo   = trim (liPoNo item)
-       }
-
-fixItem :: LineItem -> LineItem
-fixItem = trimItem . fixCatNo
-
-getLineNo :: LineItem -> Maybe Integer
-getLineNo = readMaybe . liLineNo
-
-sortItems :: [LineItem] -> [LineItem]
-sortItems = sortBy (comparing getLineNo)
+writeLabel :: LineItem -> IO ()
+writeLabel item = do
+  let destFile = destDir ++ liPoNo item ++ "-" ++ liLineNo item ++ ".label"
+  makeLabel destFile item
 
 main :: IO ()
 main = do
   items <- lineItemsFromFile htmlfile
+  items' <- transformItems prefix items
   let items' = sortItems $ map fixItem items
-  mapM_ (putStrLn . show) items'
+  mapM_ writeLabel items'
---
+++ src/TransformItem.hs
@@ -0,0 +1,45 @@
+module TransformItem (transformItems) where
+
+import qualified Data.ByteString        as B
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Char8  as B8
+
+loadImage :: FilePath -> LineItem -> IO LineItem
+loadImage prefix item = do
+  let fname = prefix ++ liImg item
+  image <- B.readFile fname
+  let b64 = B64.encode image
+  return $ item { liImg = B8.unpack b64 }
+
+fixCatNo :: LineItem -> LineItem
+fixCatNo item@(LineItem { liCatNo = "", liGotBreak = False }) =
+  item { liDesc = ""
+       , liCatNo = liDesc item
+       }
+fixCatNo item = item
+
+trim :: String -> String
+trim s = dropWhile isSpace $ dropWhileEnd isSpace s
+
+trimItem :: LineItem -> LineItem
+trimItem item =
+  item { liTitle  = trim (liTitle item)
+       , liDesc   = trim (liDesc item)
+       , liCatNo  = trim (liCatNo item)
+       , liLineNo = trim (liLineNo item)
+       , liPoNo   = trim (liPoNo item)
+       }
+
+fixItem :: LineItem -> LineItem
+fixItem = trimItem . fixCatNo
+
+getLineNo :: LineItem -> Maybe Integer
+getLineNo = readMaybe . liLineNo
+
+sortItems :: [LineItem] -> [LineItem]
+sortItems = sortBy (comparing getLineNo)
+
+transformItems :: FilePath -> [LineItem] -> IO [LineItem]
+transformItems prefix items = do
+  let items' = sortItems $ map fixItem items
+  mapM (loadImage prefix) items'
