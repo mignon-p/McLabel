7b26d559c2b64b1edef90765f307b83b47fbc8f8
Patrick Pelletier <code@funwithsoftware.org>
2021-01-16 11:37:22 -0800

    Pass srcFile and destDir as arguments.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List
+import System.Environment
 import Text.Printf
 import Text.Read
 
@@ -7,14 +9,13 @@ import MakeLabel
 import McMC
 import TransformItem
 
-prefix :: FilePath
-prefix = "/Users/ppelleti/orders/McMaster-Carr/"
-
 htmlfile :: FilePath
-htmlfile = prefix ++ "0112ppelleti.html"
+htmlfile = "/Users/ppelleti/orders/McMaster-Carr/0112ppelleti.html"
 
-destDir :: FilePath
-destDir = "/Users/ppelleti/Documents/DYMO Label Software/Labels/"
+getLabelDir :: IO FilePath
+getLabelDir = do
+  home <- getEnv "HOME"
+  return $ ensureSlash home ++ "Documents/DYMO Label Software/Labels"
 
 fmtInt :: Int -> String
 fmtInt n = printf "%03d" n
@@ -25,14 +26,28 @@ zeroPad lineNo =
     Nothing -> lineNo
     Just n -> fmtInt n
 
-writeLabel :: LineItem -> IO ()
-writeLabel item = do
+writeLabel :: FilePath -> LineItem -> IO ()
+writeLabel destDir item = do
   let lineNo = zeroPad (liLineNo item)
   let destFile = destDir ++ "Mc" ++ liPoNo item ++ "-" ++ lineNo ++ ".label"
+  putStrLn $ "Writing " ++ destFile
   makeLabel destFile item
+
+processFile :: FilePath -> FilePath -> IO ()
+processFile srcFile destDir = do
+  putStrLn $ "Reading " ++ srcFile
+  let srcDir = dropWhileEnd (/= '/') srcFile
+  items <- lineItemsFromFile srcFile
+  items' <- transformItems srcDir items
+  mapM_ (writeLabel destDir) items'
+
+ensureSlash :: FilePath -> FilePath
+ensureSlash "" = ""
+ensureSlash dir
+  | '/' == last dir = dir
+  | otherwise = dir ++ "/"
 
 main :: IO ()
 main = do
-  items <- lineItemsFromFile htmlfile
-  items' <- transformItems prefix items
-  mapM_ writeLabel items'
+  labelDir <- getLabelDir
+  processFile htmlfile (ensureSlash labelDir)
